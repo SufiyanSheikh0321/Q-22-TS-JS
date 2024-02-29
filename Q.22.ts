af57a2028c065d3d7a2a838e71c5720073c4a510
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 20:12:55 +0500

    Add files via upload

---
+++ Q.22.ts
@@ -0,0 +1,18 @@
+//22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+const Months: string[] =[
+    "January",
+    "Februray",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+console.log(Months[13]);
+console.log(Months[4]);
